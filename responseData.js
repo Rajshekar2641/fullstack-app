6a86c2d6a39172f2ce106992db4dcc8ede677d26
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-03-03 00:09:37 -0600

    Added the treasure App

---
+++ responseData.js
@@ -0,0 +1,5 @@
+let response = {
+    colorComponent1: "Hello! The target location is ",
+    colorComponent2Success: "Congratulations!! You have reached Quest: ",
+    colorComponent2failure: "You haven't reached the quest"
+}

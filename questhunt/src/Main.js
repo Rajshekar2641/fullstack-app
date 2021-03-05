4f8d6326db2d2fa8779719e6956b09d9dfff1b03
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2021-03-05 01:09:28 -0600

    added matter in main.js

--- questhunt/src/Main.js
+++ questhunt/src/Main.js
@@ -7,6 +7,8 @@ class Main extends Component {
         <div>
           <h1>Quest Hunt</h1>
           Welcome to the home page of Quest Game!!
+          The main objective of this project is to understand the concept of full stack application development. This application is all about playing a quest game where it has two colors. When the user touches the first color, it would request a valid location. Based on the location when user touches second color if they are inside the treasure location,
+they will hear and see confirmation message that he has reached the location, if user is outside the location, they will hear some information about how to reach the treasure location.
           <div className="content">
              
              </div>

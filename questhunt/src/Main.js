ce2db9d67c5cb8c7898ca1fc4cf4bdc3a6ec4764
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2021-03-05 01:07:20 -0600

    added main.js file

--- questhunt/src/Main.js
+++ questhunt/src/Main.js
@@ -1,1 +1,16 @@
 //import
+import React, { Component } from "react";
+ 
+class Main extends Component {
+  render() {
+    return (
+        <div>
+          <h1>Quest Hunt</h1>
+          Welcome to the home page of Quest Game!!
+          <div className="content">
+             
+             </div>
+           </div>
+       );
+     }
+   }

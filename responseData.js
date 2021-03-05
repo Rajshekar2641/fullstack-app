0da70080524f5631c02a04e3aa95af33e6d0642a
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-03-04 20:38:12 -0600

    Removed the existing app

--- responseData.js
+++
@@ -1,5 +0,0 @@
-let response = {
-    colorComponent1: "Hello! The target location is ",
-    colorComponent2Success: "Congratulations!! You have reached Quest: ",
-    colorComponent2failure: "You haven't reached the quest"
-}

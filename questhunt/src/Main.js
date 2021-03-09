bd0bbce45deab05bc2b61870e42aac4e9080cc33
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-03-08 18:20:19 -0600

    Added the app logo

--- questhunt/src/Main.js
+++ questhunt/src/Main.js
@@ -1,19 +1,10 @@
-//import
 import React, { Component } from "react";
- 
+
 class Main extends Component {
-  render() {
-    return (
-        <div>
-          <h1>Quest Hunt</h1>
-          Welcome to the home page of Quest Game!!
-          The main objective of this project is to understand the concept of full stack application development. This application is all about playing a quest game where it has two colors. When the user touches the first color, it would request a valid location. Based on the location when user touches second color if they are inside the treasure location,
-they will hear and see confirmation message that he has reached the location, if user is outside the location, they will hear some information about how to reach the treasure location.
-          <div className="content">
-             
-             </div>
-           </div>
-       );
-     }
-   }
-   export default Main;
+    render() {
+      return (
+          <p> This is the main page</p>
+      );
+    }
+  }
+  export default Main;

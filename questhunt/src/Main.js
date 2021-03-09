018bb7822f45d8650f287bd2028c85aed8043973
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2021-03-08 20:07:17 -0600

    added code

--- questhunt/src/Main.js
+++ questhunt/src/Main.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import "./styles/index.css";
 
 class Main extends Component {
     render() {
       return (
-          <p> This is the main page</p>
-      );
-    }
+          //<html>
+              //<body>
+                  //<main>
+                      //<article>
+                    <div className = "header" id = "header">
+                        <h1>Quest Hunt</h1>
+                        <br></br><br></br>
+                    </div>
+      )
   }
-  export default Main;
+
+  }

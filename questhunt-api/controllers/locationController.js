5a1b72d5ad4bd36da316a80f138fb75b646a8497
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-03-14 14:27:02 -0500

    Added the api to get all locations

--- questhunt-api/controllers/locationController.js
+++ questhunt-api/controllers/locationController.js
@@ -1,1 +1,40 @@
-//Added the location controller
+"use strict";
+
+const express=require('express')
+const app=express.Router()
+var LocationSchema = require('../models/location')
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+exports.create = (req, res) => {
+    const location = new LocationSchema({
+        locationName: req.body.locationName,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        radius: 100,
+      });
+     location.save(err => {
+             if(err) {
+                let status = err.status || err.statusCode || err.code || 500;
+        res.status(status).send({ status, error: err });
+             }
+                 res.send({ status: 200, response: "Location Create Successfully" });
+      } )
+}
+
+// List all the locations
+
+exports.findAll = (req, res) => {
+    LocationSchema.find()
+    .then(locations => {
+        res.send(locations);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Something wrong while retrieving locations."
+        });
+    });
+}

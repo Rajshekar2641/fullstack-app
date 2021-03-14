5dabfa1095fa0fa01a69b0ba382e50460830c0ae
Ravichanderreddy-goli <60166223+Ravichanderreddy-goli@users.noreply.github.com>
2021-03-14 14:22:45 -0500

    Update router.js

--- questhunt-api/routes/router.js
+++ questhunt-api/routes/router.js
@@ -1,1 +1,10 @@
-//Added the router file
+"use strict";
+
+var express = require('express')
+var router = express.Router();
+
+var locationController = require('../controllers/locationController')
+
+ router.get('/location/', locationController.findAll)                 
+
+module.exports =  router ;

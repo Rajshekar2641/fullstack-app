dfef86e79ac3b25423588c5991ed7d7c817f058b
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-03-17 00:24:39 -0500

    post method

--- questhunt-api/controllers/locationController.js
+++ questhunt-api/controllers/locationController.js
@@ -5,8 +5,34 @@ const app=express.Router()
 var LocationSchema = require('../models/location')
 let bodyParser = require('body-parser');
 
+
+// app.use(bodyParser.urlencoded());
+
+// app.use(bodyParser.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Add a location
+
+exports.create = (req, res) => {
+    console.log("Entered inside post",req);
+    const location = new LocationSchema({
+        locationName:req.body.locationName,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        radius: 50,
+      });
+      console.log("created location schema");
+     location.save(err => {
+             if(err) {
+                let status = err.status || err.statusCode || err.code || 500;
+        res.status(status).send({ status, error: err });
+             }
+                 res.send({ status: 200, response: "Added location succesfully" });
+      } )
+}
+
 
 // List all the locations
 
@@ -20,3 +46,28 @@ exports.findAll = (req, res) => {
         });
     });
 }
+
+// Get loaction by Id
+
+
+exports.findOne = (req, res) => {
+    LocationSchema.findById(req.params.locationId)
+    .then(location => {
+        if(!location) {
+            return res.status(404).send({
+                message: "Location 404  " + req.params.locationId
+            });            
+        }
+        res.send(location);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "location not found with id " + req.params.locationId
+            });                
+        }
+        return res.status(500).send({
+            message: "Something wrong retrieving location with id " + req.params.locationId
+        });
+    });
+}
+
--- questhunt-api/routes/location_router.js
+++ questhunt-api/routes/location_router.js
@@ -5,6 +5,8 @@ var router = express.Router();
 
 var locationController = require('../controllers/locationController')
 
- router.get('/locations', locationController.findAll)              
+ router.get('/locations', locationController.findAll)
+ router.post('/addlocation/', locationController.create)  
+ router.get('/locations/:locationId', locationController.findOne)            
 
 module.exports =  router ;

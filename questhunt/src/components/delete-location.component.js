02b0c929c252ca474ca971e08b8dc4621fab87e0
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-03-23 12:16:53 -0500

    Added delete component

--- questhunt/src/components/delete-location.component.js
+++ questhunt/src/components/delete-location.component.js
@@ -1,1 +1,128 @@
-//delete
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class DeleteLocation extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeLocationName = this.onChangeLocationName.bind(this);
+        this.onChangeLocationLatitude = this.onChangeLocationLatitude.bind(this);
+        this.onChangeLocationLongitude = this.onChangeLocationLongitude.bind(this);
+        this.onChangeLocationRadius = this.onChangeLocationRadius.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        
+
+        this.state = {
+            locationName: '',
+            latitude: '',
+            longitude: '',
+            radius: ''
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/locations/'+this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    locationName: response.data.locationName,
+                    latitude: response.data.latitude,
+                    longitude: response.data.longitude,
+                    radius: response.data.radius
+                })   
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    onChangeLocationName(e) {
+        this.setState({
+            locationName: e.target.value
+        });
+    }
+
+    onChangeLocationLatitude(e) {
+        this.setState({
+            latitude: e.target.value
+        });
+    }
+
+    onChangeLocationLongitude(e) {
+        this.setState({
+            longitude: e.target.value
+        });
+    }
+
+    onChangeLocationRadius(e) {
+        this.setState({
+            radius: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const obj = {
+            locationName: this.state.locationName,
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            radius: this.state.radius
+        };
+        console.log(obj);
+        axios.delete('http://localhost:4000/locations/delete/'+this.props.match.params.id, obj)
+            .then(res => console.log(res.data));
+        
+        this.props.history.push('/locations');
+    }
+
+    render() {
+        return (
+            <div>
+                <h3 align="center">Update Location</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group"> 
+                        <label>Location Name: </label>
+                        <input  type="text"
+                                className="form-control"
+                                value={this.state.locationName}
+                                onChange={this.onChangeLocationName} readOnly
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Latitude: </label>
+                        <input 
+                                type="text" 
+                                className="form-control"
+                                value={this.state.latitude}
+                                onChange={this.onChangeLocationLatitude} readOnly
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Longitude: </label>
+                        <input 
+                                type="text" 
+                                className="form-control"
+                                value={this.state.longitude}
+                                onChange={this.onChangeLocationLongitude} readOnly
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Radius: </label>
+                        <input 
+                                type="text" 
+                                className="form-control"
+                                value={this.state.radius}
+                                onChange={this.onChangeLocationRadius} readOnly
+                                />
+                    </div>
+
+                    <br />
+
+                    <div className="form-group">
+                        <input type="submit" value="Delete Location" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}

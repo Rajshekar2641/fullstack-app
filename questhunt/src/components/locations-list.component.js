a7b83ff5567b51d4e1225c1585d193d4ef04480c
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-03-23 12:16:29 -0500

    Added list of all locations

--- questhunt/src/components/locations-list.component.js
+++ questhunt/src/components/locations-list.component.js
@@ -1,1 +1,65 @@
-//list
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Location = props => (
+    <tr>
+        <td>{props.location.locationName}</td>
+        <td>{props.location.latitude}</td>
+        <td>{props.location.longitude}</td>
+        <td>{props.location.radius}</td>
+        <td>
+            <Link to={"/edit/"+props.location._id}>Edit</Link>
+        </td>
+        <td>
+            <Link to={"/delete/"+props.location._id}>Delete</Link>
+        </td>
+    </tr>
+)
+
+export default class LocationsList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {locations: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/locations/')
+            .then(response => {
+                this.setState({ locations: response.data });
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+    }
+
+    locationList() {
+        return this.state.locations.map(function(currentLocation, i){
+            return <Location location={currentLocation} key={i} />;
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Locations List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }} >
+                    <thead>
+                        <tr>
+                            <th>Location Name</th>
+                            <th>Latitude</th>
+                            <th>Longitude</th>
+                            <th>Radius</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.locationList() }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}

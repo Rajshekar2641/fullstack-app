72720a50e585806b0e8240d0349db516f7f5d829
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-03-22 22:05:33 -0500

    Re-established mongo connection

--- questhunt-api/locationController.js
+++ questhunt-api/locationController.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/locations', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
 
 locationRoutes.route('/').get(function(req, res) {
     Location.find(function(err, locations) {
@@ -13,4 +21,9 @@ locationRoutes.route('/').get(function(req, res) {
             res.json(locations);
         }
     });
+});
+
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
 });

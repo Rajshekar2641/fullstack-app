1c868e1ed05da3042b5be5625dd2f7b0fa032581
Ravichanderreddy-goli <60166223+Ravichanderreddy-goli@users.noreply.github.com>
2021-03-22 22:03:36 -0500

    Added the route to find all locations

--- questhunt-api/locationController.js
+++ questhunt-api/locationController.js
@@ -1,1 +1,16 @@
-//Added the controller file
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+
+locationRoutes.route('/').get(function(req, res) {
+    Location.find(function(err, locations) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(locations);
+        }
+    });
+});

c3d0108959868c77c4d68e8f9af86fef8e520c7d
Ravichanderreddy-goli <60166223+Ravichanderreddy-goli@users.noreply.github.com>
2021-03-30 18:45:03 -0500

    Added .env configuration

--- questhunt-api/app.js
+++ questhunt-api/app.js
@@ -1,19 +1,29 @@
-var express = require('express');
-var app = express();
-var port = 3000;
-var mongoose = require("mongoose")
-app.use(require('./routes/location_router'))
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+require('dotenv').config();
 
-mongoose.connect('mongodb+srv://s538099:Raj@2641@cluster0.ghisa.azure.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-{ useNewUrlParser: true, useUnifiedTopology: true }).then((res) =>{
-  app.listen(3000, function () {
-  return "Connected to Database"
-  
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(uri, { useNewUrlParser: true });
+
+const connection= mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongodb database connection established successfully !!");
 })
-}).catch((e) => {
- console.log(e,"error")
-})
+
+const locationsRouter = require('./controllers/locationController');
+
+
+app.use('/locations', locationsRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});

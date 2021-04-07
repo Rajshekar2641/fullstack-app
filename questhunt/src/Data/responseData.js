12b501bdc1a810f080e6f3530d91c67758776017
GANDE RAJASHEKAR <60024301+Rajshekar2641@users.noreply.github.com>
2021-04-07 00:48:07 -0500

    Update responseData.js

--- questhunt/src/Data/responseData.js
+++ questhunt/src/Data/responseData.js
@@ -1,5 +1,5 @@
 export const response = [{
     colorComponent1: "Hello! The random location choosen is ",
     colorComponent2Success: "Congratulations!! You have reached Quest: ",
-    colorComponent2failure: "You haven't reached the quest"
+    colorComponent2failure: "You haven't reached the quest. Go on you can do this!!!!!!"
 }];
